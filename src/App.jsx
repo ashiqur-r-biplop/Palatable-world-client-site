d1451e68ccef4f900968640019f79c129972ef40
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-05-02 10:55:40 +0600

    install react router , tailwind daysi ui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+
+
+const App = () => {
+  
+  return (
+    <div>
+      <h1>hi</h1>
+      <button className="btn btn-ghost">Button</button>
+      
+    </div>
+  );
+};
+
+export default App;

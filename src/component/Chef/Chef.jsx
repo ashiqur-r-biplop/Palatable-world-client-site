efde1e44d6f9ee5b856bd2da39362c13fc431ae2
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-05-02 15:41:38 +0600

    handle sell all and show less btn

--- src/component/Chef/Chef.jsx
+++ src/component/Chef/Chef.jsx
@@ -3,16 +3,21 @@ import React, { useEffect, useState } from "react";
 const Chef = () => {
   const [sixChefData, setSixChefData] = useState([]);
   const [fullChefData, setFullChefData] = useState([]);
+  const [lessChef, setLessChef] = useState([]);
   useEffect(() => {
     fetch("https://palatable-world-client-site-ashiqur23.vercel.app/chef/all")
       .then((res) => res.json())
       .then((data) => {
         setSixChefData(data.slice(0, 6));
+        setLessChef(data.slice(0, 6));
         setFullChefData(data);
       });
   }, []);
   const handleSeeAll = () => {
     setSixChefData(fullChefData);
+  };
+  const handleShowLess = () => {
+    setSixChefData(lessChef);
   };
   return (
     <div>
@@ -26,21 +31,27 @@ const Chef = () => {
               <img src={chef?.chefPhoto} alt="Shoes" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">{chef?.chefName}</h2>
-              <p>recipes quantity : {chef?.recipes_quantity}</p>
-              <p>Total like : {chef?.total_like}</p>
-              <p>Years of experience : {chef?.years_of_experience}</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">View Recipes</button>
+              <h2 className="card-title text-2xl">{chef?.chefName}</h2>
+              <p><span className="font-semibold">recipes quantity :</span> {chef?.recipes_quantity}</p>
+              <p><span className="font-semibold">Total like :</span> {chef?.total_like}</p>
+              <p><span className="font-semibold">Years of experience :</span> {chef?.years_of_experience}</p>
+              <div className="card-actions justify-start">
+                <button className="btn-primary bottom-0">View Recipes</button>
               </div>
             </div>
           </div>
         ))}
       </div>
       <div className="text-center">
-        <button onClick={handleSeeAll} className="btn-primary">
-          See All
-        </button>
+        {sixChefData.length > 6 ? (
+          <button onClick={handleShowLess} className="btn-primary">
+            show Less
+          </button>
+        ) : (
+          <button onClick={handleSeeAll} className="btn-primary">
+            See All Chef
+          </button>
+        )}
       </div>
     </div>
   );
